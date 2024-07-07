a3ef61033caf95e64f0885c6202292d15632a4d3
rnjsmj <rnjsmj0820@naver.com>
2024-07-07 22:57:33 +0900

    sample test 완료
    sample list 성공

--- redclip/src/main/java/com/kh/redclip/sample/model/service/SampleService.java
+++ redclip/src/main/java/com/kh/redclip/sample/model/service/SampleService.java
@@ -2,11 +2,9 @@ package com.kh.redclip.sample.model.service;
 
 import java.util.List;
 
-import org.springframework.stereotype.Service;
 
 import com.kh.redclip.sample.model.vo.Sample;
 
-@Service
 public interface SampleService {
 	
 	List<Sample> findAll();
