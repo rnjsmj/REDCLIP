7cde85e75cbedae5060e00b3ba4f75197c35912b
rnjsmj <rnjsmj0820@naver.com>
2024-07-07 23:42:39 +0900

    Revert "Merge branch 'main' of https://github.com/rnjsmj/REDCLIP"
    This reverts commit 085766ab7ba83c1481834002a31aade98e165c43, reversing
    changes made to 45cabc1e60a4ab4955d2f9db47292edf211384aa.

--- redclip/src/main/java/com/kh/redclip/sample/controller/SampleController.java
+++ redclip/src/main/java/com/kh/redclip/sample/controller/SampleController.java
@@ -5,7 +5,6 @@ import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.kh.redclip.sample.model.service.SampleService;
@@ -29,12 +28,7 @@ public class SampleController {
 		
 		model.addAttribute("list", sampleList);
 		return "sample/list";
-  }
-	
-	@GetMapping("/{id}")
-	public String findById() {
-		
-		return "sample/list";
 	}
+	
 	
 }
