2b63db2964a734fdbe0b1d2dcd9063b6229abf3f
jungjung12 <h2o2345@naver.com>
2024-07-07 23:03:55 +0900

    Revert "Update SampleController.java"
    This reverts commit e6c8a0a91b34f58b79a30484291bdb20981cf02d.

--- redclip/src/main/java/com/kh/redclip/sample/controller/SampleController.java
+++ redclip/src/main/java/com/kh/redclip/sample/controller/SampleController.java
@@ -5,7 +5,6 @@ import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.kh.redclip.sample.model.service.SampleService;
@@ -28,12 +27,6 @@ public class SampleController {
 		List<Sample> sampleList = sampleService.findAll();
 		
 		model.addAttribute("list", sampleList);
-		return "sample/list";
-	}
-	
-	@GetMapping("/{id}")
-	public String findById() {
-		
 		return "sample/list";
 	}
 	
