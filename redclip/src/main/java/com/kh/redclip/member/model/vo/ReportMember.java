047f886f3117607e563045905effc3e823462a35
jungjung12 <h2o2345@naver.com>
2024-07-08 16:16:03 +0900

    신고회원 클래스 내용 작성함!
    꼬밋

--- redclip/src/main/java/com/kh/redclip/member/model/vo/ReportMember.java
+++ redclip/src/main/java/com/kh/redclip/member/model/vo/ReportMember.java
@@ -1,5 +1,21 @@
 package com.kh.redclip.member.model.vo;
 
+import java.sql.Date;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
 public class ReportMember {
 
+	private int reportNo;			//신고 번호
+	private String reportTitle;		//신고 제목
+	private String reportContent;	//신고 내용
+	private Date reportDate;		//신고 일자
+	private String reportedId;		//신고당한 사람
 }
