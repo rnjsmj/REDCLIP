f5d3203df0645f17faca7d3082e5c67b8e2fe980
tjdgk2019 <160584411+tjdgk2019@users.noreply.github.com>
2024-07-08 12:03:19 +0900

    Revert "vo barter"
    This reverts commit 46d47454fd16064ff448a30d7c67d16cb872a999.

--- redclip/src/main/java/com/kh/redclip/barter/model/vo/Barter.java
+++ redclip/src/main/java/com/kh/redclip/barter/model/vo/Barter.java
@@ -1,12 +1,5 @@
 package com.kh.redclip.barter.model.vo;
 
 public class Barter {
-	private int barterNo;
-	private String barterContent;
-	private String barterWriter;
-	private String villageCode;
-	private String category;
-	private int hit;
-	private String barterDate;
-	private String barterStatus;
+	
 }
