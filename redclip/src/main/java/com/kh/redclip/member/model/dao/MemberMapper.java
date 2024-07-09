00e632825e74cc1b8f52c2f7aee6f889f1b0828e
jongwooleezzang <whddn1310@naver.com>
2024-07-09 21:06:46 +0900

    07/09 21:00 이종우 커밋
    커밋

--- redclip/src/main/java/com/kh/redclip/member/model/dao/MemberMapper.java
+++ redclip/src/main/java/com/kh/redclip/member/model/dao/MemberMapper.java
@@ -1,8 +1,12 @@
 package com.kh.redclip.member.model.dao;
 
+import java.util.List;
+import java.util.Map;
+
 import org.apache.ibatis.annotations.Mapper;
 
 import com.kh.redclip.member.model.vo.Member;
+import com.kh.redclip.region.model.vo.Region;
 
 @Mapper
 public interface MemberMapper {
@@ -11,4 +15,13 @@ public interface MemberMapper {
 	int checkNick(String userNick);
 
 	Member login(Member member);
+
+	List<Region> selectgu(int cityCode);
+	
+	List<Region> selectdong(int townCode);
+
+	int insertMember(Member member);
+
+
+	
 }
--- redclip/src/main/java/com/kh/redclip/member/model/service/MemberService.java
+++ redclip/src/main/java/com/kh/redclip/member/model/service/MemberService.java
@@ -1,6 +1,10 @@
 package com.kh.redclip.member.model.service;
 
+import java.util.List;
+import java.util.Map;
+
 import com.kh.redclip.member.model.vo.Member;
+import com.kh.redclip.region.model.vo.Region;
 
 public interface MemberService {
 	int idCheck(String userId);
@@ -8,4 +12,12 @@ public interface MemberService {
 	int nickCheck(String userNick);
 
 	Member login(Member member);
+
+
+
+	List<Region> selectgu(int cityCode);
+
+	List<Region> selectdong(int townCode);
+
+	int insert(Member member);
 }
