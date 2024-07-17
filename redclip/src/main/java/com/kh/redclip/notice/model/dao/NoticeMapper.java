1fc24003fab16ad0d2e1922e0973088a26fa8818
jongwooleezzang <whddn1310@naver.com>
2024-07-17 16:40:49 +0900

    7/17 이종우커밋
    커밋

--- redclip/src/main/java/com/kh/redclip/notice/model/dao/NoticeMapper.java
+++ redclip/src/main/java/com/kh/redclip/notice/model/dao/NoticeMapper.java
@@ -1,5 +1,16 @@
 package com.kh.redclip.notice.model.dao;
 
+import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
+
+import com.kh.redclip.notice.model.vo.Notice;
+
+@Mapper
 public class NoticeMapper {
 
+	public int noticeCount;
+	
+	public List<Notice> selectNotice;
+	
 }
