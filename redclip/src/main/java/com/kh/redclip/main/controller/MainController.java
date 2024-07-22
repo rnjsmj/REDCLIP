175f293f6535939bca7482887fa3fcde0a6f8788
jongwooleezzang <whddn1310@naver.com>
2024-07-22 23:43:12 +0900

    0722 이종우 커밋
    커밋

--- redclip/src/main/java/com/kh/redclip/main/controller/MainController.java
+++ redclip/src/main/java/com/kh/redclip/main/controller/MainController.java
@@ -22,10 +22,14 @@ public class MainController {
 	private final MainService mainService;
 	
 	//최근상품
-	@GetMapping("/")
+	@GetMapping("/redclip")  //서버를 켰을때 생기는 url을 여기에 매핑해줘야 내가원하는 서버run >컨트롤러 > 리턴 > 
+	//질문 서버를 켰을때 생기는 url은 뭐고???   :http://localhost:8080/redclip/ <=(톰캣서버 path)
+	// 왜 그 url은 view밖에있는 index를 찾아가는가 ?
+	
+	// 왜 webapp 안에있으면 인덱스를 바로탐??? 뭐때매???
     public String recently(Model model, BarterVO barterVo) {
 		
-		log.info("haha");
+		log.info("진짜 시팔");
       List<BarterVO> recentProducts = mainService.getRecentProducts();
       model.addAttribute("recentProducts", recentProducts);
       log.info("BarterVO 빠라빠라빠라 빰빰빰  {}",recentProducts);
